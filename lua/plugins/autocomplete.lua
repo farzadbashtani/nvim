29dc3e56eb8977d522d209729fb98595f26ece23
Farzad Bashtani <farzadbashtani@live.com>
2025-03-18 17:55:53 -0600

    fix autocomplete config and keybinds

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -3,8 +3,17 @@ return {
         "saghen/blink.cmp",
         opts = function(_, opts)
             opts.keymap = {
-                preset = "super-tab",
-                ["C-y"] = { "select_and_accept" },
+                preset = "none",
+                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-e>"] = { "cancel", "fallback" },
+                ["<C-y>"] = { "select_and_accept" },
+
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+                ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+                ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
             }
         end,
     },
