c4a8947beac69eecbc5813349f9a5a902ca06cee
Farzad Bashtani <farzadbashtani@live.com>
2025-04-10 20:06:24 -0600

    config changes to lazyvim
    show hidden files by default
    disable inlay-hints by default
    disable line diagnostic text (only show an icon)

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = {
+                enabled = false,
+            },
+            diagnostics = {
+                virtual_text = false,
+                underline = false,
+            },
+        },
+    },
+}
