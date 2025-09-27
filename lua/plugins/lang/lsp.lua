bd16124d951224ab8a2c794d107f82a32c358e5e
Farzad Bashtani <farzadbashtani@live.com>
2025-09-27 11:37:28 -0600

    remove tailwind-tools which breaks lspconfig

--- lua/plugins/lang/lsp.lua
+++ lua/plugins/lang/lsp.lua
@@ -160,15 +160,4 @@ return {
 		end,
 	},
 	{ "tpope/vim-rails" },
-	{
-		"luckasRanarison/tailwind-tools.nvim",
-		name = "tailwind-tools",
-		build = ":UpdateRemotePlugins",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {},
-	},
 }
